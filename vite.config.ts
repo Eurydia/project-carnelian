7ce8001229485cec84c4d3e9844feeecb8104e12
Eurydia <crossdeltavi@gmail.com>
2025-03-16 14:03:05 +0100

    fixed issue when searching for attendees check in will not work correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPath from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPath()],
   base: "/project-carnelian/",
 });
 
